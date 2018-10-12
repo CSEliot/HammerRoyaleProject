caa46a741a8234295d72067a988a78e3b19a8d1f
Eliot L. Carney-Seim <ecsit39@gmail.com>
2018-10-11 22:36:39 -0400

    Preliminary plugins added.

---
+++ Assets/Scripts/HitTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTrigger : MonoBehaviour {
+    public CharacterControls parent;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.CompareTag("PlayerTrigger") && collider.tag.Contains("Player") && collider.gameObject.GetComponent<CharacterControls>() != parent)
+            parent.punchTargets.Add(collider.gameObject.GetComponent<CharacterControls>());
+        CBUG.Do("TriggerEnter: " + collider.name);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (!collider.CompareTag("PlayerTrigger") && collider.tag.Contains("Player") && collider.gameObject.GetComponent<CharacterControls>() != parent)
+            parent.punchTargets.Remove(collider.gameObject.GetComponent<CharacterControls>());
+        CBUG.Do("TriggerExit: " + collider.name);
+    }
+}
